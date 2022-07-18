aceb25f8b07dee0678a3658f6720d11a71eea1c6
Jan Ekr <108271285+ekrja@users.noreply.github.com>
2022-07-18 08:15:03 +0200

    OslServerProcess class has been implemented.

--- src/ansys/optislang/core/__init__.py
+++ src/ansys/optislang/core/__init__.py
@@ -4,9 +4,19 @@ optiSLang.
 core
 """
 
+import os
+import sys
+
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
+
+IRON_PYTHON = sys.platform == "cli"
+WINDOWS = os.name == "nt"
+LINUX = os.name == "posix"
+PY3 = sys.version_info[0] >= 3
+
+from ansys.optislang.core.osl_process import OslServerProcess
---
+++ src/ansys/optislang/core/encoding.py
@@ -0,0 +1,47 @@
+"""Contains utility functions for encoding and decoding text."""
+
+import sys
+
+from ansys.optislang.core import PY3
+
+defenc = sys.getdefaultencoding()
+
+if PY3:
+    text_type = str
+    binary_type = bytes
+
+else:
+    if defenc == "ascii":
+        defenc = "utf-8"
+    text_type = unicode
+    binary_type = str
+
+string_types = (binary_type,)
+
+
+def safe_decode(s):
+    """Safely decodes a binary_type to text_type."""
+    if isinstance(s, text_type):
+        return s
+    elif isinstance(s, string_types):
+        return s.decode(defenc, "surrogateescape")
+    elif s is not None:
+        raise TypeError("Expected bytes or text, but got %r" % (s,))
+
+
+def safe_encode_to_ascii(text: str) -> str:
+    """Safely converts text encoding to ASCII standard.
+
+    Conversion is performed ignoring the unencodable unicode characters.
+
+    Parameters
+    ----------
+    text : str
+        String which is supposed to be encoded to ASCII standard.
+
+    Returns
+    -------
+    str
+        String encoded in ASCII standard.
+    """
+    return text.encode("ascii", "ignore").decode("ascii")
