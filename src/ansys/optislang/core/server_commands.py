5ee6b9cde364ec7aacc143e553e931d8fdb1b048
PanekOndrej <108389964+PanekOndrej@users.noreply.github.com>
2023-08-16 17:21:00 +0200

    Omitted check of uid&hid in start server command.

--- src/ansys/optislang/core/server_commands.py
+++ src/ansys/optislang/core/server_commands.py
@@ -1302,9 +1302,9 @@ def start(actor_uid: str = None, hid: str = None, password: str = None) -> str:
     Parameters
     ----------
     actor_uid: str, opt
-        Actor uid entry.
+        Actor uid entry. A Hirearchical ID (hid) is required.
     hid: str, opt
-        Hid entry.
+        Hid entry. The actor uid is required.
     password : str, opt
         Password.
 
@@ -1313,6 +1313,11 @@ def start(actor_uid: str = None, hid: str = None, password: str = None) -> str:
     str
         JSON string of ``start`` command.
     """
+    if actor_uid and hid is None:
+        raise ValueError("The Hirearchical ID (hid) is required.")
+    elif actor_uid is None and hid:
+        raise ValueError("The actor uid is required.")
+
     return _to_json(
         _gen_server_command(command=_START, actor_uid=actor_uid, hid=hid, password=password)
     )
