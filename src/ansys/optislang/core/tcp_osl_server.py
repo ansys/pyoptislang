86aafe69e85fc16a31c3b684daa708ba80d8d828
Ondrej <108389964+PanekOndrej@users.noreply.github.com>
2023-11-10 14:30:15 +0100

    Add `service` arg.

--- src/ansys/optislang/core/tcp_osl_server.py
+++ src/ansys/optislang/core/tcp_osl_server.py
@@ -825,6 +825,15 @@ class TcpOslServer(OslServer):
         Defaults to ``None``.
     batch : bool, optional
         Determines whether to start optiSLang server in batch mode. Defaults to ``True``.
+
+        ..note:: Cannot be used in combination with service mode.
+
+    service: bool, optional
+        Determines whether to start optiSLang server in service mode. If ``True``,
+        ``batch`` argument is set to ``False``. Defaults to ``False``.
+
+        ..note:: Cannot be used in combination with batch mode.
+
     port_range : Tuple[int, int], optional
         Defines the port range for optiSLang server. Defaults to ``None``.
     no_run : bool, optional
@@ -966,6 +975,7 @@ class TcpOslServer(OslServer):
         executable: Optional[Union[str, Path]] = None,
         project_path: Optional[Union[str, Path]] = None,
         batch: bool = True,
+        service: bool = False,
         port_range: Optional[Tuple[int, int]] = None,
         no_run: Optional[bool] = None,
         no_save: bool = False,
@@ -977,7 +987,7 @@ class TcpOslServer(OslServer):
         ini_timeout: float = 20,
         password: Optional[str] = None,
         logger=None,
-        shutdown_on_finished=True,
+        shutdown_on_finished: bool = True,
         env_vars: Optional[Mapping[str, str]] = None,
         import_project_properties_file: Optional[Union[str, Path]] = None,
         export_project_properties_file: Optional[Union[str, Path]] = None,
@@ -1001,6 +1011,7 @@ class TcpOslServer(OslServer):
         self.__executable = Path(executable) if executable is not None else None
         self.__project_path = Path(project_path) if project_path is not None else None
         self.__batch = batch
+        self.__service = service
         self.__port_range = port_range
         self.__no_run = no_run
         self.__no_save = no_save
@@ -2311,6 +2322,7 @@ class TcpOslServer(OslServer):
                 shutdown_on_finished=shutdown_on_finished,
                 logger=self._logger,
                 batch=self.__batch,
+                service=self.__service,
                 port_range=self.__port_range,
                 no_run=self.__no_run,
                 force=self.__force,
