411baf939aa19446fa49ee056b9187ffa7d55d6c
Markus Schubert <markus.schubert@ansys.com>
2024-04-23 08:17:47 +0200

    Skip more tests for 23R2

--- tests/tcp/test_tcp_osl_server.py
+++ tests/tcp/test_tcp_osl_server.py
@@ -32,6 +32,7 @@ import uuid
 import pytest
 
 from ansys.optislang.core import OslServerProcess, errors
+from ansys.optislang.core.osl_server import OslVersion
 import ansys.optislang.core.tcp.osl_server as tos
 
 _host = socket.gethostbyname(socket.gethostname())
@@ -1020,6 +1021,9 @@ def test_save_copy(
     path_type,
 ):
     """Test ``save_copy``."""
+    if optislang.osl_version < OslVersion(24, 1, 0, 0):
+        pytest.skip(f"Not compatible with {optislang.osl_version_string}")
+
     copy_path = tmp_path / "test_save_copy.opf"
     arg_path = path_type(copy_path)
 
