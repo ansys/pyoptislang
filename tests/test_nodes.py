b2e10b0f5b817d03b0afe2aad6d5120b8955c2f5
Markus Schubert <markus.schubert@ansys.com>
2024-05-13 06:37:32 +0200

    Remove unused paremeterization dimension

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -63,80 +63,80 @@ def from_str_invalid_inputs_method(
 
 
 @pytest.mark.parametrize(
-    "design_flow, name",
+    "name",
     [
-        (DesignFlow, "NONE"),
-        (DesignFlow, "RECEIVE"),
-        (DesignFlow, "SEND"),
-        (DesignFlow, "RECEIVE_SEND"),
+        "NONE",
+        "RECEIVE",
+        "SEND",
+        "RECEIVE_SEND",
     ],
 )
-def test_design_flow(design_flow: DesignFlow, name: str):
+def test_design_flow(name: str):
     """Test `DesignFlow`."""
-    enumeration_test_method(enumeration_class=design_flow, enumeration_name=name)
+    enumeration_test_method(enumeration_class=DesignFlow, enumeration_name=name)
 
 
 @pytest.mark.parametrize(
-    "node_class_type, name",
+    "name",
     [
-        (NodeClassType, "NODE"),
-        (NodeClassType, "SYSTEM"),
-        (NodeClassType, "PARAMETRIC_SYSTEM"),
-        (NodeClassType, "ROOT_SYSTEM"),
-        (NodeClassType, "INTEGRATION_NODE"),
+        "NODE",
+        "SYSTEM",
+        "PARAMETRIC_SYSTEM",
+        "ROOT_SYSTEM",
+        "INTEGRATION_NODE",
     ],
 )
-def test_node_class_type(node_class_type: NodeClassType, name: str):
+def test_node_class_type(name: str):
     """Test `NodeClassType`."""
-    enumeration_test_method(enumeration_class=node_class_type, enumeration_name=name)
+    enumeration_test_method(enumeration_class=NodeClassType, enumeration_name=name)
 
 
 @pytest.mark.parametrize(
-    "sampling_type, name",
+    "name",
     [
-        (SamplingType, "CENTERPOINT"),
-        (SamplingType, "FULLFACTORIAL"),
-        (SamplingType, "AXIAL"),
-        (SamplingType, "STARPOINTS"),
-        (SamplingType, "KOSHAL"),
-        (SamplingType, "CENTRALCOMPOSITE"),
-        (SamplingType, "MIXEDTERMS"),
-        (SamplingType, "LATINHYPER"),
-        (SamplingType, "LATINHYPERDETEMINISTIC"),
-        (SamplingType, "OPTIMIZEDLATINHYPER"),
-        (SamplingType, "ORTHOLATINHYPERDETEMINISTIC"),
-        (SamplingType, "SOBOLSEQUENCES"),
-        (SamplingType, "PLAINMONTECARLO"),
-        (SamplingType, "DOPTIMAL"),
-        (SamplingType, "DOPTIMALLINEAR"),
-        (SamplingType, "DOPTIMALQUADRATIC"),
-        (SamplingType, "DOPTIMALQUADRATICNOMIXED"),
-        (SamplingType, "KOSHALLINEAR"),
-        (SamplingType, "KOSHALQUADRATIC"),
-        (SamplingType, "FEKETE"),
-        (SamplingType, "BOXBEHNKEN"),
-        (SamplingType, "FULLCOMBINATORIAL"),
-        (SamplingType, "ADVANCEDLATINHYPER"),
+        "CENTERPOINT",
+        "FULLFACTORIAL",
+        "AXIAL",
+        "STARPOINTS",
+        "KOSHAL",
+        "CENTRALCOMPOSITE",
+        "MIXEDTERMS",
+        "LATINHYPER",
+        "LATINHYPERDETEMINISTIC",
+        "OPTIMIZEDLATINHYPER",
+        "ORTHOLATINHYPERDETEMINISTIC",
+        "SOBOLSEQUENCES",
+        "PLAINMONTECARLO",
+        "DOPTIMAL",
+        "DOPTIMALLINEAR",
+        "DOPTIMALQUADRATIC",
+        "DOPTIMALQUADRATICNOMIXED",
+        "KOSHALLINEAR",
+        "KOSHALQUADRATIC",
+        "FEKETE",
+        "BOXBEHNKEN",
+        "FULLCOMBINATORIAL",
+        "ADVANCEDLATINHYPER",
     ],
 )
-def test_sampling_type(sampling_type: SamplingType, name: str):
+def test_sampling_type(name: str):
     """Test `SamplingType`."""
-    enumeration_test_method(enumeration_class=sampling_type, enumeration_name=name)
+    enumeration_test_method(enumeration_class=SamplingType, enumeration_name=name)
 
 
 @pytest.mark.parametrize(
-    "slot_type, name, direction",
+    "name, direction",
     [
-        (SlotType, "INPUT", "receiving"),
-        (SlotType, "OUTPUT", "sending"),
-        (SlotType, "INNER_INPUT", "receiving"),
-        (SlotType, "INNER_OUTPUT", "sending"),
+        ("INPUT", "receiving"),
+        ("OUTPUT", "sending"),
+        ("INNER_INPUT", "receiving"),
+        ("INNER_OUTPUT", "sending"),
     ],
 )
-def test_slot_type(slot_type: SlotType, name: str, direction: str):
+def test_slot_type(name: str, direction: str):
     """Test `SlotType`."""
-    output = enumeration_test_method(enumeration_class=slot_type, enumeration_name=name)
-    assert slot_type.to_dir_str(output) == direction
+    output = enumeration_test_method(enumeration_class=SlotType, enumeration_name=name)
+    assert SlotType.to_dir_str(output) == direction
 
 
 @pytest.mark.parametrize(
