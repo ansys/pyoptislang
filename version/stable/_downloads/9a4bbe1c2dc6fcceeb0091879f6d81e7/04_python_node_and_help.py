bc7e09ea0d553fd343fed5b20a059805f5f96cb8
optislangdeveloper <optislangdeveloper@users.noreply.github.com>
2024-10-08 15:01:56 +0000

    DOC: update development documentation c6ff3b648f5f597bb27c04f2ebc7203479ff10f6

---
+++ version/stable/_downloads/9a4bbe1c2dc6fcceeb0091879f6d81e7/04_python_node_and_help.py
@@ -0,0 +1,97 @@
+# Copyright (C) 2022 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+.. _ref_python_node_and_help:
+
+Python node and help
+--------------------
+
+This example demonstrates how to create workflow components and configure them.
+
+It uses the ``python_node.py`` file to create a Python2 node.
+and perform these tasks:
+
+- Adds some source code.
+- Adds and removes parameters and responses.
+- Adds internal variables.
+
+It then uses the ``python_help.py`` file to print which nodes are
+available in the Python console. Lastly, it explains
+how you can optionally save the project to a desired location.
+"""
+
+#########################################################
+# Perform required imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Perform the required imports.
+
+from ansys.optislang.core import Optislang
+import ansys.optislang.core.examples as examples
+
+#########################################################
+# Create optiSLang instance
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# Create the optiSLang instance.
+
+osl = Optislang(ini_timeout=60)
+print(osl)
+
+#########################################################
+# Get paths of example scripts and run them
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get the paths of the example scripts and then run
+# these scripts.
+
+paths1 = examples.get_files("python_node")
+paths2 = examples.get_files("python_help")
+osl.application.project.run_python_file(paths1[0])
+print(osl.application.project.run_python_file(paths2[0]))
+
+#########################################################
+# Optionally save project
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# If you want to save the project to some desired
+# location, uncomment and edit these lines:
+#
+# .. code:: python
+#
+#   path = r'<insert-desired-location>'
+#   osl.application.save_as(os.path.join(path, "test_project.opf"))
+#
+
+#########################################################
+# Stop and cancel project
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Stop and cancel the project.
+
+osl.dispose()
+
+#########################################################
+# View generated workflow
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# This image shows the generated workflow.
+#
+# .. image:: ../../_static/04_python_node_and_help.png
+#  :width: 300
+#  :alt: Result of script.
+#
