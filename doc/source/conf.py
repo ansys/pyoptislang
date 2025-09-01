e999fdefa045a7cf114d9d283da5952025c56084
Markus Schubert <markus.schubert@ansys.com>
2025-09-01 11:17:05 +0200

    Format

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -162,7 +162,5 @@ html_context = {
     "github_repo": "pyoptislang",
     "github_version": "main",
     "doc_path": "doc/source",
-    "pyansys_tags": [
-        "Connect"
-    ]
+    "pyansys_tags": ["Connect"],
 }
