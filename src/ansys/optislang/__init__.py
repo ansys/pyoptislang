b4c39c581190f41af142a9b3676508ff0f8da635
Ronny Fahlberg <86288939+rfahlberg@users.noreply.github.com>
2026-01-14 12:58:08 +0100

    Maint/fix missing init module (#534)

--- src/ansys/optislang/__init__.py
+++ src/ansys/optislang/__init__.py
@@ -20,40 +20,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
-from pathlib import Path
-from typing import OrderedDict
+"""
+ansys.
 
-import pytest
-
-from ansys.optislang.core import utils
-
-pytestmark = pytest.mark.local_osl
+optislang
+"""
 
 
-def test_get_awp_root_dirs():
-    "Test that function returns dictionary of matching environment variables."
-    environ_vars = list(utils.iter_awp_roots())
-    # Unified installer doesn't set AWP_ROOT by default on Linux
-    if os.name != "posix":
-        assert len(environ_vars) > 0
+from importlib import metadata as _metadata
 
+_DISTRIBUTION_NAME = "ansys-optislang-core"
 
-def test_get_osl_executable():
-    "Test that function returns executable, if exists."
-    auto_exe_path = utils.get_osl_exec()
-    assert isinstance(auto_exe_path, tuple)
-    assert isinstance(auto_exe_path[0], int)
-    assert isinstance(auto_exe_path[1], Path)
-    exe_path = utils.get_osl_exec(osl_version="999")
-    assert exe_path is None
+try:
+    __version__ = _metadata.version(_DISTRIBUTION_NAME)
+except _metadata.PackageNotFoundError:
+    __version__ = "0.0.dev0"
 
-
-def test_find_all_osl_exec():
-    "Test that function returns OrderedDict."
-    dictionary = utils.find_all_osl_exec()
-    version = next(iter(dictionary.keys()))
-    assert isinstance(dictionary, OrderedDict)
-    assert isinstance(version, int)
-    assert isinstance(dictionary[version][0], Path)
-    assert isinstance(dictionary[version], (Path, tuple))
+__all__ = ["core"]
